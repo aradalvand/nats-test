4acc0b1461dec23b1b7a9d31fe4e76dcaf30d3f2
Arad Alvand <aradalvand@gmail.com>
2024-10-31 00:39:20 +0330

    Initial

---
+++ consumer/Program.cs
@@ -0,0 +1,11 @@
+﻿using NATS;
+using NATS.Net;
+
+await using var client = new NatsClient();
+await foreach (var message in client.SubscribeAsync<int>("Shenas.Otps.*"))
+{
+    Console.WriteLine($"Message received: {message.Data}");
+}
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
---
+++ producer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using NATS;
+using NATS.Net;
+
+await using var client = new NatsClient();
+while (true)
+{
+    Stopwatch sw = new();
+    Console.Write("Partition Key: ");
+    var partitionKey = Console.ReadLine()!;
+    sw.Start();
+    await client.PublishAsync($"Shenas.Otps.{partitionKey}", new Random().Next(1, 9));
+    sw.Stop();
+    Console.WriteLine($"Message sent in {sw.Elapsed.TotalMilliseconds:N3} ms");
+    Console.WriteLine("---");
+
+    Console.CancelKeyPress += async (e, b) =>
+    {
+        Console.WriteLine("Disposing client and producer.");
+        await client.DisposeAsync();
+        Console.WriteLine("Disposed client and producer.");
+    };
+}
