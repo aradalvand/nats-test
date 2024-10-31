7afe79ba375869f07e2ba1cf08cba6e0184b56a8
Arad Alvand <aradalvand@gmail.com>
2024-10-31 12:43:14 +0330

    with serialization

--- consumer/Program.cs
+++ consumer/Program.cs
@@ -1,11 +1,57 @@
-﻿using NATS;
+﻿using System.Buffers;
+using System.Diagnostics;
+using System.Text.Json;
+using NATS;
+using NATS.Client.Core;
+using NATS.Client.JetStream.Models;
 using NATS.Net;
 
 await using var client = new NatsClient();
-await foreach (var message in client.SubscribeAsync<int>("Shenas.Otps.*"))
+var jetStream = client.CreateJetStreamContext();
+var stream = await jetStream.CreateStreamAsync(new(
+    "TEST",
+    ["Shenas.Otps.>"]
+)
 {
-    Console.WriteLine($"Message received: {message.Data}");
+    MaxMsgsPerSubject = 1,
+    Discard = StreamConfigDiscard.New,
+    DiscardNewPerSubject = true,
+});
+var consumer = await stream.CreateOrUpdateConsumerAsync(new("WhyShouldINameTheConsumer"));
+Console.WriteLine("Stream and consumer created — now consuming...");
+await foreach (var message in consumer.ConsumeAsync(new Ser()))
+{
+    Console.WriteLine($"Message received: {message.Subject} - {message.Data}");
+    Stopwatch sw = new();
+    sw.Start();
+    await message.AckAsync();
+    sw.Stop();
+    Console.WriteLine($"Message acknowledged in {sw.Elapsed.TotalMilliseconds:N3} ms");
+    Console.WriteLine("---");
 }
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+public class Ser : INatsSerializer<Foo>
+{
+    public INatsSerializer<Foo> CombineWith(INatsSerializer<Foo> next)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Foo? Deserialize(in ReadOnlySequence<byte> buffer)
+    {
+        // todo: inefficient
+        var value = JsonSerializer.Deserialize<Foo>(buffer.ToArray());
+        return value;
+    }
+
+    public void Serialize(IBufferWriter<byte> bufferWriter, Foo value)
+    {
+        var bytes = JsonSerializer.SerializeToUtf8Bytes(value);
+        bufferWriter.Write(bytes);
+    }
+}
+
+public record Foo(
+    int X,
+    int Y
+);
--- producer/Program.cs
+++ producer/Program.cs
@@ -1,5 +1,7 @@
-﻿using System.Diagnostics;
-using NATS;
+﻿using System.Buffers;
+using System.Diagnostics;
+using System.Text.Json;
+using NATS.Client.Core;
 using NATS.Net;
 
 await using var client = new NatsClient();
@@ -9,7 +11,9 @@ while (true)
     Console.Write("Partition Key: ");
     var partitionKey = Console.ReadLine()!;
     sw.Start();
-    await client.PublishAsync($"Shenas.Otps.{partitionKey}", new Random().Next(1, 9));
+    // NOTE: The publishing side of nats is extremely simple — no knowledge of streams — we simply publish a message to a specific subject. See https://youtu.be/EJJ2SG-cKyM?t=260
+    var foo = new Foo(123, 321);
+    await client.PublishAsync($"Shenas.Otps.{partitionKey}", foo, serializer: new Ser());
     sw.Stop();
     Console.WriteLine($"Message sent in {sw.Elapsed.TotalMilliseconds:N3} ms");
     Console.WriteLine("---");
@@ -21,3 +25,29 @@ while (true)
         Console.WriteLine("Disposed client and producer.");
     };
 }
+
+public class Ser : INatsSerializer<Foo>
+{
+    public INatsSerializer<Foo> CombineWith(INatsSerializer<Foo> next)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Foo? Deserialize(in ReadOnlySequence<byte> buffer)
+    {
+        // todo: inefficient
+        var value = JsonSerializer.Deserialize<Foo>(buffer.ToArray());
+        return value;
+    }
+
+    public void Serialize(IBufferWriter<byte> bufferWriter, Foo value)
+    {
+        var bytes = JsonSerializer.SerializeToUtf8Bytes(value);
+        bufferWriter.Write(bytes);
+    }
+}
+
+public record Foo(
+    int X,
+    int Y
+);
